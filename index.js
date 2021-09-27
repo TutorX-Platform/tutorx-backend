82f0d7fbfdb60c7630a7feb0dc84bfaea4374f89
sandun.m <sandun.madampage@scicom.com.my>
2021-09-27 21:59:14 +0530

    fix: comment off cors

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ const hbs = require('nodemailer-express-handlebars');
 const DeviceDetector = require("device-detector-js");
 
 // middleware
-var whitelist = ['https://tutetory.com']
-var corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
+// var whitelist = ['https://tutetory.com']
+// var corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 
 
 
